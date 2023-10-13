c0f35b6c7195aa3c87b35bc6229282adccaa8c36
Pelotfr <polomeignan@hotmail.fr>
2023-10-13 16:44:28 +0200

    Removed non essential unit tests

--- src/tests/index.spec.ts
+++ src/tests/index.spec.ts
@@ -7,38 +7,6 @@ import { banner } from "../banner";
 const dbIsUp = (await fetch(`${config.DB_HOST}/ping`).catch((error) => {}))?.status == 200;
 console.info(`Database is ${dbIsUp ? '' : 'not '}running !`);
 
-describe('Index page (/)', () => {
-    it('Should return 200 Response', async () => {
-        const res = await app.request('/');
-        expect(res.status).toBe(200);
-    });
-
-    it('Should have the banner as the body', async () => {
-        const res = await app.request('/');
-        expect(await res.text()).toBe(banner());
-    });
-});
-
-describe('Health page (/health)', () => {
-    it.skipIf(dbIsUp)('Should fail on database connection error', async () => {
-        const res = await app.request('/health');
-        expect(res.status).toBe(503);
-
-        const json = await res.json();
-        expect(json).toHaveProperty('db_status');
-        expect(json.db_status).toContain('ConnectionRefused');
-    });
-
-    it.if(dbIsUp)('Should return 200 Response', async () => {
-        const res = await app.request('/health');
-        expect(res.status).toBe(200);
-
-        const json = await res.json();
-        expect(json).toHaveProperty('db_status');
-        expect(json.db_status).toContain('Ok');
-    });
-});
-
 describe('Sales count query page (/salescount?collection_name=<string>)', () => {
     it('Should fail on missing collection name parameter', async () => {
         const res = await app.request('/salescount');
@@ -49,7 +17,9 @@ describe('Sales count query page (/salescount?collection_name=<string>)', () => 
     });
 
     it('Should fail on non-valid collection name parameter', async () => {
-        const res = await app.request('/salescount?collection_name=123');
+        // make an int collection name
+        let collection_name = 123;
+        const res = await app.request(`/totalvolume?collection_name=${collection_name}`);
         expect(res.status).toBe(500);
 
         const json = await res.json();
@@ -84,7 +54,8 @@ describe('Total volume query page (/totalvolume?collection_name=<string>)', () =
     });
 
     it('Should fail on non-valid collection name parameter', async () => {
-        const res = await app.request('/totalvolume?collection_name=123');
+        let collection_name = 123;
+        const res = await app.request(`/totalvolume?collection_name=${collection_name}`);
         expect(res.status).toBe(500);
 
         const json = await res.json();
