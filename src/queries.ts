57a9be614f7a5ed9a85f02bba972901ed0a42200
Pelotfr <polomeignan@hotmail.fr>
2023-10-13 15:38:38 +0200

    Added untracked files

---
+++ src/queries.ts
@@ -0,0 +1,17 @@
+import config from './config'
+
+async function makeQuery(query: string) {
+    const response = await fetch(`${config.DB_HOST}/?default_format=JSONCompact`, {
+        method: "POST",
+        body: query,
+        headers: { "Content-Type": "text/plain" },
+    });
+
+    return await response.json();
+}
+
+export async function salesCountQuery(collection_name: string) {
+    const query = `SELECT COUNT(sale_id) FROM ${config.DB_NAME} WHERE (collection_name == ${collection_name})`;
+
+    return await makeQuery(query);
+}
