59d3ac9391459807d30a3ce859f9c78aec00ade1
Denis Carriere <carriere.denis@gmail.com>
2023-10-25 16:23:42 -0400

    set client as read only

--- src/clickhouse/createClient.ts
+++ src/clickhouse/createClient.ts
@@ -1,11 +1,12 @@
 import { createClient } from "@clickhouse/client-web";
-import { ping } from "./ping";
-import { APP_NAME, config } from "../config";
+import { ping } from "./ping.js";
+import { APP_NAME, config } from "../config.js";
 
 const client = createClient({
     ...config,
     clickhouse_settings: {
         allow_experimental_object_type: 1,
+        readonly: "1",
     },
     application: APP_NAME,
 })
